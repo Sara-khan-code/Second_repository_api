27ea3227702e1803c6b7a89bd8916e4bba1c4628
Sara Nargis <saranargis79@gmail.com>
2024-02-15 20:16:48 +0500

    new changes .gitignore

--- second_apifile.py
+++ second_apifile.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/user/<name>")
+
+def user(name):
+    return render_template("user.template", user_name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
